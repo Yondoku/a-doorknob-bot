20e5f3e60a1fde7ba6b8e6904768544773ada65f
Mars _YT _YT <abcandy654@gmail.com>
2022-02-23 06:10:18 +0000

    woo! permissions fix!!!!!!!!!

--- commands/webhook_create.js
+++ commands/webhook_create.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { WebhookClient } = require('discord.js');
+const { WebhookClient, Permissions } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -10,6 +10,7 @@ module.exports = {
 			.setDescription('input: webhook name')
 			.setRequired(true)),
 	async run(interaction) {
+			if (!interaction.member.has(Permissions.FLAGS.MANAGE_WEBHOOKS)) return await interaction.reply("You need the Manage Webhooks permission to use this command.");
 		let input = interaction.options.getString('input');
 		interaction.channel.createWebhook(input).then(async e => {await interaction.reply({content:`Webhook created! Pass the link ${e.url} to the webhook_send command to send messages with the webhook!`,ephemeral:true})});
 	},
--- commands/webhook_send.js
+++ commands/webhook_send.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { WebhookClient } = require('discord.js');
+const { WebhookClient, Permissions } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -13,6 +13,7 @@ module.exports = {
 			.setDescription('input: text to send to the webhook')
 			.setRequired(true)),
 	async run(interaction) {
+		if (!interaction.member.has(Permissions.FLAGS.MANAGE_WEBHOOKS)) return await interaction.reply("You need the Manage Webhooks permission to use this command.")
 		let link = interaction.options.getString('link'), input = interaction.options.getString('input');
 		const webhookClient = new WebhookClient({url: link});
 		webhookClient.send(input);
